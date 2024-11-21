0d66ae0eb32e2da5115f79ea53296baa3e4f563e
Anknguyen <97307551+Anknguyen@users.noreply.github.com>
2024-11-21 15:32:02 -0500

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const textElement = screen.getByText(/Test App/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(textElement).toBeInTheDocument();
 });
